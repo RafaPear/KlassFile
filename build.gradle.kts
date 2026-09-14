bcbd49056895fb3b3734f49a71d7e8ba3c7dcb32
Rafael Pereira <rafapear@outlook.pt>
2026-09-14 11:46:24 +0100

    fix: handle potential null values for centralUsername and centralPassword in publication configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ java {
 
 val javadocJar = tasks.register<Jar>("javadocJar") {
     description = "Assembles Javadoc jar"
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
     dependsOn(tasks.dokkaGenerateHtml)
     archiveClassifier.set("javadoc")
     from(tasks.dokkaGenerateHtml)
@@ -71,8 +72,8 @@ publishing {
 
 nmcp {
     publishAllPublications {
-        username = providers.gradleProperty("centralUsername").get()
-        password = providers.gradleProperty("centralPassword").get()
+        username = providers.gradleProperty("centralUsername").orNull
+        password = providers.gradleProperty("centralPassword").orNull
         publicationType = "AUTOMATIC"
     }
 }
